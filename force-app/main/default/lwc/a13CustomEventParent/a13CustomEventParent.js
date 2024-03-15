a447db90e0159f03a2c5267b752e98cb10a0fde9
baskivani <burhanzeylan@gmail.com>
2024-03-15 20:54:59 +0300

    commit

---
+++ force-app/main/default/lwc/a13CustomEventParent/a13CustomEventParent.js
@@ -0,0 +1,10 @@
+import { LightningElement } from 'lwc';
+
+export default class A13CustomEventParent extends LightningElement {
+
+    bilgi=''
+    handleElma(){
+this.bilgi='Bakin ben geldim'
+
+    }
+}
---
+++ force-app/main/default/lwc/a13childBir/a13childBir.js
@@ -0,0 +1,11 @@
+import { LightningElement } from 'lwc';
+
+export default class A13childBir extends LightningElement {
+
+    yeniBilgi=''
+    
+    handleClick(){
+
+
+    }
+}
